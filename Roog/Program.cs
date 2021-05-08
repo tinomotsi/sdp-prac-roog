329340d9222ba48983d88fca16e863d7a2add0c8
Tino Tino Tino <tino@whynot.com>
2021-05-08 23:24:23 +0200

    Changes from task 2 number 11

--- Roog/Program.cs
+++ Roog/Program.cs
@@ -6,7 +6,8 @@ namespace Roog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+			Console.WriteLine("Tinotenda Motsi");
         }
     }
 }
